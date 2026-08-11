c5f4ff2445ac7443c7d44fe78f7c1ddb6e5cc7b2
ayeshamufliha <ayeshamufliha.s@gmail.com>
2026-08-11 22:40:55 +0530

    Add files via upload

---
+++ program for fizz buzz.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 18:51:12 2026
+
+@author: Admin
+"""
+
+for i in range(1,51):
+    if(i%3==0 and i%5==0):
+        print("fizzbuzz")
+    elif(i%3==0):
+       print("fizz")
+    elif(i%5==0):
+        print("buzz")
+    else:
+        print(i)
