a48172fb59d3f3432f646703c9cf8bbdfa7c0a33
ayeshamufliha <ayeshamufliha.s@gmail.com>
2026-08-11 22:42:10 +0530

    Add files via upload

---
+++ count the number od vowels.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  6 12:01:55 2026
+
+@author: Admin
+"""
+
+s='thaarani'
+count=0
+vowels = ['a','e','i','o','u']
+for i in s:
+    if i in vowels:
+     count+=1
+print(count)    
